148a4126ad73881a2c141e32b264d11f60caf7da
jahid11223 <jahidhasan1695@gmail.com>
2022-03-26 10:29:35 +0600

    login screen fixed

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,25 +1,26 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { userLoginReducer } from "./Reducers/userReducers";
 
 
 const reducer = combineReducers({
 
-  //userLogin: userLoginReducer,
+  userLogin: userLoginReducer,
 
 });
 
 
 // login
-// const userInfoFromLocalStorage = localStorage.getItem("userInfo")
-//   ? JSON.parse(localStorage.getItem("userInfo"))
-//   : null;
+const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 
 
 
 const initialState = {
 
-  //userLogin: { userInfo: userInfoFromLocalStorage },
+  userLogin: { userInfo: userInfoFromLocalStorage },
 };
 
 const middleware = [thunk];
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/LoadingError/Error";
+import { login } from './../Redux/Actions/userActions';
+import Loading from './../components/LoadingError/Loading';
 
-const Login = () => {
+const Login = ({history}) => {
+
+  window.scrollTo(0, 0);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { error, loading, userInfo } = userLogin;
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push("/");
+    }
+  }, [userInfo, history]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
   return (
     <>
       <div
@@ -8,13 +33,21 @@ const Login = () => {
         style={{ maxWidth: "380px", marginTop: "100px" }}
       >
         <div className="card-body">
+          {
+            error && <Message variant="alert-danger">{error}</Message>
+          }
+          {
+            loading && <Loading />
+          }
           <h4 className="card-title mb-4 text-center">Sign in</h4>
-          <form>
+          <form onSubmit={submitHandler}>
             <div className="mb-3">
               <input
                 className="form-control"
                 placeholder="Email"
                 type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="mb-3">
@@ -22,6 +55,8 @@ const Login = () => {
                 className="form-control"
                 placeholder="Password"
                 type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
 
