18d2e561a014ea5f68ff92c179cf6ac77ebdce86
jahid11223 <jahidhasan1695@gmail.com>
2022-03-26 09:28:13 +0600

    admin dashboard clean

---
+++ src/Redux/store.js
@@ -0,0 +1,33 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+
+const reducer = combineReducers({
+
+  //userLogin: userLoginReducer,
+
+});
+
+
+// login
+// const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+//   ? JSON.parse(localStorage.getItem("userInfo"))
+//   : null;
+
+
+
+const initialState = {
+
+  //userLogin: { userInfo: userInfoFromLocalStorage },
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
